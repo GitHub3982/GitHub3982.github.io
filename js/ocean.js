70e2bde03bd69ff06ee8f80cebf76aa9033fa020
GitHub3982 <917003982@qq.com>
2019-09-25 11:37:45 +0800

    Site updated: 2019-09-25 11:37:26

--- js/ocean.js
+++ js/ocean.js
@@ -209,3 +209,12 @@
 })(jQuery);
 
 
+//vv
+$(document).ready(function(){
+    //$('.vemoji-btn').text('😀');
+    $("#vcomments").on('click', 'span.vat',function(){
+        $(this).parent('div.vmeta').next("div.vcontent").after($("div.vwrap"));
+        $('textarea#veditor').focus();
+    })
+})
+

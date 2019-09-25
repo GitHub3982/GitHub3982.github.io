3ad572c4c0bb0ef9cf14bce7a728dd6581429c46
GitHub3982 <917003982@qq.com>
2019-09-25 13:10:37 +0800

    Site updated: 2019-09-25 13:10:33

--- js/vv.js
+++ js/vv.js
@@ -1,6 +1,6 @@
         new Valine({
             el: '#vcomments',
             appId: '6kIHCBGKCOhwhK0sb4mjGows-MdYXbMMI',
-            appKey: 'lMU1tflmyocAB0inEJx3pdSD'
+            appKey: 'lMU1tflmyocAB0inEJx3pdSD',
 			visitor: true // 阅读量统计
         })
